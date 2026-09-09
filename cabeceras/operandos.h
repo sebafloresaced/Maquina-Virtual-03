a73a85032bc9e87fb34f4427f7c0339d5d7d54e7
Lopez Tomas Gustavo <tomilopez555@gmail.com>
2026-09-09 03:30:06 +0000

    añado funcion para cargar tipos de operando

--- cabeceras/operandos.h
+++ cabeceras/operandos.h
@@ -1,1 +1,6 @@
-// Obtiene el valor de un operando y guarda resultados en registros o memoria, según su tipo.
+// Obtiene el tipo de operandos y la operacion, y los carga en registros
+
+#include <stdint.h>
+#include "../cabeceras/maquina.h"
+
+void desensamblaInstruccion(MaquinaVirtual *maquina, uint8_t instruccion, int *desplazamiento);
