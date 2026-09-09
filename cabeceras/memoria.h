f1450b6dd0ab6943edcb4a33d7251a491272196b
Lopez Tomas Gustavo <tomilopez555@gmail.com>
2026-09-09 01:36:40 +0000

    Añado calculo y validacion de direccion fisica

--- cabeceras/memoria.h
+++ cabeceras/memoria.h
@@ -1,1 +1,5 @@
-// Traduce las direcciones según la segmentación y controla las lecturas y escrituras dentro de los límites válidos.
+// Traduce las direcciones según la segmentación y controla las lecturas y escrituras dentro de los límites válidos, retorna una direccion fisica VALIDA.
+#include <stdint.h>
+#include "../cabeceras/maquina.h"
+
+uint32_t direccionFisica(MaquinaVirtual *, uint32_t, int);
---
+++ fuentes/memoria.c
@@ -0,0 +1,18 @@
+#include "../cabeceras/memoria.h"
+
+uint32_t DireccionFisica(MaquinaVirtual *maquina, uint32_t direccionLogica, uint32_t cant_bytes_acceso){
+    uint16_t offset, segmento;
+    uint32_t DireccionFisica, limite, base;
+
+    segmento = direccionLogica >> 16;
+    base = (maquina->segmentos[segmento].base << 16);
+    limite = maquina->segmentos[segmento].tamanio + base;
+    offset = direccionLogica & 0x0000FFFF;
+
+    DireccionFisica = base + offset;
+
+    if (DireccionFisica >= base && DireccionFisica+cant_bytes_acceso <= limite)
+        return DireccionFisica;
+    else
+        return 0; //esto es error
+}
