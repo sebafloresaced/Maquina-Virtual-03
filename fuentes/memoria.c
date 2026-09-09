be03dbdcb32325e6a7196b71d339f46ee1ac6d4e
Lopez Tomas Gustavo <tomilopez555@gmail.com>
2026-09-09 02:14:16 +0000

    correccion en calculos de la funcion

--- fuentes/memoria.c
+++ fuentes/memoria.c
@@ -2,16 +2,17 @@
 
 uint32_t DireccionFisica(MaquinaVirtual *maquina, uint32_t direccionLogica, uint32_t cant_bytes_acceso){
     uint16_t offset, segmento;
-    uint32_t DireccionFisica, limite, base;
+    uint32_t DireccionFisica, limiteSegmento, limiteAcceso, base;
 
     segmento = direccionLogica >> 16;
     base = maquina->segmentos[segmento].base;
-    limite = maquina->segmentos[segmento].tamanio + base;
+    limiteSegmento = maquina->segmentos[segmento].tamanio + base;
     offset = direccionLogica & 0x0000FFFF;
 
     DireccionFisica = base + offset;
+    limiteAcceso = DireccionFisica + cant_bytes_acceso;
 
-    if (DireccionFisica >= base && DireccionFisica + cant_bytes_acceso <= limite)
+    if (DireccionFisica >= base &&  limiteAcceso<= limiteSegmento)
         return DireccionFisica;
     else
         return 0; //esto es error no esta en el segmento
