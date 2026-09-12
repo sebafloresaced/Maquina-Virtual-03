0dd34c182a3423e567e344aa8534dc6f049ef148
sebafloresaced <sebafloresaced@gmail.com>
2026-09-12 03:31:18 +0000

    Reemplazo manejo de errores en la carga del archivo por exit

--- fuentes/cargador.c
+++ fuentes/cargador.c
@@ -2,48 +2,54 @@
 #include <string.h>
 #include "../cabeceras/cargador.h"
 
-static int leerEncabezado(FILE *archivo) {
+static void leerEncabezado(FILE *archivo) {
   uint8_t encabezado[8];
   int tamanioCodigo;
 
   if (fread(encabezado, 1, 8, archivo) != 8) { // no tiene los 8 bytes del encabezado
     printf("Error: encabezado incompleto\n");
-    return -1;
+    exit(EXIT_FAILURE);
   }
+
   if (memcmp(encabezado, "VMX26", 5) != 0) { // no cumple el identificador del traductor
     printf("Error: identificador incorrecto\n");
-    return -1;
+    exit(EXIT_FAILURE);
   }
+
   if (encabezado[5] != 1) { // no cumple la version del traductor
     printf("Error: version incorrecta\n");
-    return -1;
+    exit(EXIT_FAILURE);
   }
+
   tamanioCodigo = ((uint16_t)encabezado[6] << 8) | encabezado[7];
   if (tamanioCodigo > TAM_MEMORIA) { // consultar si tirar error o cargar el codigo que entre en memoria
     printf("Error: el codigo no entra en la memoria\n");
-    return -1;
+    exit(EXIT_FAILURE);
   }
+
   return tamanioCodigo;
 }
 
-int cargarPrograma(const char *nombreArchivo, MaquinaVirtual *maquina) {
+void cargarPrograma(const char *nombreArchivo, MaquinaVirtual *maquina) {
   FILE *archivo;
   int tamanioCodigo;
 
   archivo = fopen(nombreArchivo, "rb");
+
   if (archivo == NULL) {
     printf("No se pudo abrir el archivo\n");
-    return 0;
+    exit(EXIT_FAILURE);
   }
+
   tamanioCodigo = leerEncabezado(archivo);
   if (tamanioCodigo == -1) { // hubo un error con el encabezado
     fclose(archivo);
-    return 0;
+    exit(EXIT_FAILURE);
   }
   if (fread(maquina->memoria, 1, tamanioCodigo, archivo) != (size_t)tamanioCodigo) { // no se pudieron leer la cantidad de bytes indicada
     printf("Error: el archivo contiene menos codigo del indicado\n");
     fclose(archivo);
-    return 0;
+    exit(EXIT_FAILURE);
   }
 
   /* Segmento de codigo */
@@ -58,5 +64,4 @@ int cargarPrograma(const char *nombreArchivo, MaquinaVirtual *maquina) {
   maquina->registros[IP] = maquina->registros[CS];
 
   fclose(archivo);
-  return 1;
 }
