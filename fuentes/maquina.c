cf8e458502eb8dc1687be9bdb38dbcf54e09d749
sebafloresaced <sebafloresaced@gmail.com>
2026-09-12 03:30:42 +0000

    Modifico nombres de operaciones

--- fuentes/maquina.c
+++ fuentes/maquina.c
@@ -3,9 +3,7 @@
 
 void inicializarMaquina(MaquinaVirtual *maquina) {
     int i;
-    for (i = 0; i < TAM_MEMORIA; i++) { // consultar si hacer o no
-        maquina->memoria[i] = 0;
-    }
+    
     for (i = 0; i < CANT_REGISTROS; i++) {
         maquina->registros[i] = 0;
     }
@@ -15,34 +13,34 @@ void inicializarMaquina(MaquinaVirtual *maquina) {
     }
 
     // operaciones
-    maquina->Operaciones[MOV] = mov;
-    maquina->Operaciones[ADD] = add;
-    maquina->Operaciones[SUB] = sub;
-    maquina->Operaciones[MUL] = mul;
-    maquina->Operaciones[DIV] = divi;
-    maquina->Operaciones[CMP] = cmp;
-    maquina->Operaciones[AND] = and;
-    maquina->Operaciones[OR] = or;
-    maquina->Operaciones[XOR] = xor;
-    maquina->Operaciones[SWAP] = swap;
-    maquina->Operaciones[SHL] = shl;
-    maquina->Operaciones[SHR] = shr;
-    maquina->Operaciones[SAR] = sar;
-    maquina->Operaciones[LDL] = ldl;
-    maquina->Operaciones[LDH] = ldh;
-    maquina->Operaciones[RND] = rnd;
+    maquina->Operaciones[MOV] = operacionMOV;
+    maquina->Operaciones[ADD] = operacionADD;
+    maquina->Operaciones[SUB] = operacionSUB;
+    maquina->Operaciones[MUL] = operacionMUL;
+    maquina->Operaciones[DIV] = operacionDIV;
+    maquina->Operaciones[CMP] = operacionCMP;
+    maquina->Operaciones[AND] = operacionAND;
+    maquina->Operaciones[OR] = operacionOR;
+    maquina->Operaciones[XOR] = operacionXOR;
+    maquina->Operaciones[SWAP] = operacionSWAP;
+    maquina->Operaciones[SHL] = operacionSHL;
+    maquina->Operaciones[SHR] = operacionSHR;
+    maquina->Operaciones[SAR] = operacionSAR;
+    maquina->Operaciones[LDL] = operacionLDL;
+    maquina->Operaciones[LDH] = operacionLDH;
+    maquina->Operaciones[RND] = operacionRND;
 
-    maquina->Operaciones[SYS] = sys;
-    maquina->Operaciones[JMP] = jmp;
-    maquina->Operaciones[JP] = jp;
-    maquina->Operaciones[JN] = jN;
-    maquina->Operaciones[JZ] = jz;
-    maquina->Operaciones[JC] = jc;
-    maquina->Operaciones[JV] = jv;
-    maquina->Operaciones[JNP] = jnp;
-    maquina->Operaciones[JNN] = jnn;
-    maquina->Operaciones[JNZ] = jnz;
-    maquina->Operaciones[NOT] = not;
+    maquina->Operaciones[SYS] = operacionSYS;
+    maquina->Operaciones[JMP] = operacionJMP;
+    maquina->Operaciones[JP] = operacionJP;
+    maquina->Operaciones[JN] = operacionJN;
+    maquina->Operaciones[JZ] = operacionJZ;
+    maquina->Operaciones[JC] = operacionJC;
+    maquina->Operaciones[JV] = operacionJV;
+    maquina->Operaciones[JNP] = operacionJNP;
+    maquina->Operaciones[JNN] = operacionJNN;
+    maquina->Operaciones[JNZ] = operacionJNZ;
+    maquina->Operaciones[NOT] = operacionNOT;
 
-    maquina->Operaciones[STOP] = stop;
+    maquina->Operaciones[STOP] = operacionSTOP;
 }
