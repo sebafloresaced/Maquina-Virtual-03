5c80acfd8558bf6d521681177e6f4b64b776ff04
sebafloresaced <sebafloresaced@gmail.com>
2026-09-15 01:50:44 +0000

    Desarrollo de funciones SYS

--- cabeceras/llamadas_sistema.h
+++ cabeceras/llamadas_sistema.h
@@ -3,7 +3,21 @@
 #ifndef LLAMADAS_SISTEMA_H
 #define LLAMADAS_SISTEMA_H
 
-void escribirDatos(MaquinaVirtual *maquina);
-void leerDatos(MaquinaVirtual *maquina);
+#include "maquina.h"
+#include <stdint.h>
 
-#endif 
+// Operaciones SYS
+#define LEER 1
+#define ESCRIBIR 2
+
+// Formatos
+#define DECIMAL 1
+#define CARACTER 2
+#define OCTAL 4
+#define HEXADECIMAL 8
+#define BINARIO 16
+
+void leerDatos(MaquinaVirtual *maquina);
+void escribirDatos(MaquinaVirtual *maquina);
+
+#endif
--- fuentes/llamadas_sistema.c
+++ fuentes/llamadas_sistema.c
@@ -1,14 +1,115 @@
 #include "../cabeceras/maquina.h"
 #include "../cabeceras/llamadas_sistema.h"
+#include "../cabeceras/operaciones.h"
+#include <stdio.h>
 
 // EDX -> posicion de memoria
 // EAX -> formato
 // ECX (2 bytes mas significativos) -> tamanio de valores
 // ECX (2 bytes menos significativos) -> cantidad de valores
 
-void escribirDatos(MaquinaVirtual *maquina) {
-    
-}
-void leerDatos(MaquinaVirtual *maquina) {
+// Formatos:
+// %#X -> hexadecimal
+// %d -> decimal   
+// %o -> octal
+// %#b -> binario
+// %c -> caracter
 
+void escribirDatos(MaquinaVirtual *maquina) 
+{
+    uint32_t tamanio = maquina->registros[ECX] >> 16;
+    maquina->registros[MAR] = tamanio << 16;
+
+    uint32_t formato = maquina->registros[EAX];
+    uint32_t cantidad = maquina->registros[ECX] & 0xFFFF;
+
+    switch (formato) {
+        case HEXADECIMAL:
+            for (int i = 0; i < cantidad; i++) {
+                maquina->registros[LAR] = maquina->registros[EDX] + i * tamanio;
+                leerMemoria(maquina);
+                printf("[0x%X]: 0x%X \n", maquina->registros[MAR] & 0xFFFF, maquina->registros[MBR]); // prompt
+            }
+            break;
+        case DECIMAL: 
+            for (int i = 0; i < cantidad; i++) {
+                maquina->registros[LAR] = maquina->registros[EDX] + i * tamanio;
+                leerMemoria(maquina);
+                printf("[0x%X]: %d \n", maquina->registros[MAR] & 0xFFFF, maquina->registros[MBR]);
+            }
+            break;
+        case OCTAL: 
+            for (int i = 0; i < cantidad; i++) {
+                maquina->registros[LAR] = maquina->registros[EDX] + i * tamanio;
+                leerMemoria(maquina);
+                printf("[0x%X]: 0%o \n", maquina->registros[MAR] & 0xFFFF, maquina->registros[MBR]);
+            }
+            break;
+        case BINARIO:
+            for (int i = 0; i < cantidad; i++) {
+                maquina->registros[LAR] = maquina->registros[EDX] + i * tamanio;
+                leerMemoria(maquina);
+                printf("[0x%X]: 0b%b \n", maquina->registros[MAR] & 0xFFFF, maquina->registros[MBR]);
+            }
+            break;
+        case CARACTER: 
+            for (int i = 0; i < cantidad; i++) {
+                maquina->registros[LAR] = maquina->registros[EDX] + i * tamanio;
+                leerMemoria(maquina);
+                printf("[0x%X]: %c \n", maquina->registros[MAR] & 0xFFFF, maquina->registros[MBR]);
+            }
+            break;
+    }
+}
+
+void leerDatos(MaquinaVirtual *maquina) 
+{
+    uint32_t tamanio = maquina->registros[ECX] >> 16;
+    maquina->registros[MAR] = tamanio << 16;
+
+    uint32_t formato = maquina->registros[EAX];
+    uint32_t cantidad = maquina->registros[ECX] & 0xFFFF;
+
+    switch (formato) {
+        case DECIMAL: 
+            for (int i = 0; i < cantidad; i++) {
+                scanf("%d", &maquina->registros[MBR]);
+                maquina->registros[LAR] = maquina->registros[EDX] + i * tamanio;
+                escribirMemoria(maquina);
+                printf("[0x%X]: %d \n", maquina->registros[MAR] & 0xFFFF, maquina->registros[MBR]); // prompt
+            }
+            break;
+        case CARACTER: 
+            for (int i = 0; i < cantidad; i++) {
+                scanf("%c", &maquina->registros[MBR]);
+                maquina->registros[LAR] = maquina->registros[EDX] + i * tamanio;
+                escribirMemoria(maquina);
+                printf("[0x%X]: %c \n", maquina->registros[MAR] & 0xFFFF, maquina->registros[MBR]);
+            }
+            break;
+        case OCTAL: 
+            for (int i = 0; i < cantidad; i++) {
+                scanf("%o", &maquina->registros[MBR]);
+                maquina->registros[LAR] = maquina->registros[EDX] + i * tamanio;
+                escribirMemoria(maquina);
+                printf("[0x%X]: 0%o \n", maquina->registros[MAR] & 0xFFFF, maquina->registros[MBR]);
+            }
+            break;
+        case BINARIO: 
+            for (int i = 0; i < cantidad; i++) {
+                scanf("%b", &maquina->registros[MBR]);
+                maquina->registros[LAR] = maquina->registros[EDX] + i * tamanio;
+                escribirMemoria(maquina);
+                printf("[0x%X]: 0b%b \n", maquina->registros[MAR] & 0xFFFF, maquina->registros[MBR]);
+            }
+            break;
+        case HEXADECIMAL: 
+            for (int i = 0; i < cantidad; i++) {
+                scanf("%x", &maquina->registros[MBR]);
+                maquina->registros[LAR] = maquina->registros[EDX] + i * tamanio;
+                escribirMemoria(maquina);
+                printf("[0x%X]: 0x%X \n", maquina->registros[MAR] & 0xFFFF, maquina->registros[MBR]);
+            }
+            break;
+    }
 }
