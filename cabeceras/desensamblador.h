099009fc07279fa0ee4813d0001c91e5985e2c8c
Lopez Tomas Gustavo <tomilopez555@gmail.com>
2026-09-16 00:32:24 +0000

    cambios en desensamblador.c y desensamblador.h para corregir el tipo de la variable nombreoperando de char* a st15 (un arreglo de 15 caracteres). Esto asegura que la función obtieneOperando maneje correctamente los nombres de los operandos sin problemas de memoria.

--- cabeceras/desensamblador.h
+++ cabeceras/desensamblador.h
@@ -3,6 +3,8 @@
 
 #include "maquina.h"
 
+typedef char st15[15];
+
 void desensamblador(MaquinaVirtual maquina);
 
 #endif
--- fuentes/desensamblador.c
+++ fuentes/desensamblador.c
@@ -5,94 +5,100 @@
 #include "../cabeceras/maquina.h"
 #include "../cabeceras/operaciones.h"
 #include "../cabeceras/instrucciones.h"
+#include "../cabeceras/desensamblador.h"
 
-static char *nombreOperacion(uint8_t codigo) {
+static void nombreOperacion(uint8_t codigo, st15 nombre) {
     switch (codigo) {
-        case SYS: return "SYS";
-        case JMP: return "JMP";
-        case JP:  return "JP";
-        case JN:  return "JN";
-        case JZ:  return "JZ";
-        case JC:  return "JC";
-        case JV:  return "JV";
-        case JNP: return "JNP";
-        case JNN: return "JNN";
-        case JNZ: return "JNZ";
-        case NOT: return "NOT";
-        case STOP: return "STOP";
-        case MOV: return "MOV";
-        case ADD: return "ADD";
-        case SUB: return "SUB";
-        case MUL: return "MUL";
-        case DIV: return "DIV";
-        case CMP: return "CMP";
-        case AND: return "AND";
-        case OR:  return "OR";
-        case XOR: return "XOR";
-        case SWAP: return "SWAP";
-        case SHL: return "SHL";
-        case SHR: return "SHR";
-        case SAR: return "SAR";
-        case LDL: return "LDL";
-        case LDH: return "LDH";
-        case RND: return "RND";
-        default:  return "OP DESCONOCIDA";
+        case SYS: strcpy(nombre, "SYS"); break;
+        case JMP: strcpy(nombre, "JMP"); break;
+        case JP:  strcpy(nombre, "JP"); break;
+        case JN:  strcpy(nombre, "JN"); break;
+        case JZ:  strcpy(nombre, "JZ"); break;
+        case JC:  strcpy(nombre, "JC"); break;
+        case JV:  strcpy(nombre, "JV"); break;
+        case JNP: strcpy(nombre, "JNP"); break;
+        case JNN: strcpy(nombre, "JNN"); break;
+        case JNZ: strcpy(nombre, "JNZ"); break;
+        case NOT: strcpy(nombre, "NOT"); break;
+        case STOP: strcpy(nombre, "STOP"); break;
+        case MOV: strcpy(nombre, "MOV"); break;
+        case ADD: strcpy(nombre, "ADD"); break;
+        case SUB: strcpy(nombre, "SUB"); break;
+        case MUL: strcpy(nombre, "MUL"); break;
+        case DIV: strcpy(nombre, "DIV"); break;
+        case CMP: strcpy(nombre, "CMP"); break;
+        case AND: strcpy(nombre, "AND"); break;
+        case OR:  strcpy(nombre, "OR"); break;
+        case XOR: strcpy(nombre, "XOR"); break;
+        case SWAP: strcpy(nombre, "SWAP"); break;
+        case SHL: strcpy(nombre, "SHL"); break;
+        case SHR: strcpy(nombre, "SHR"); break;
+        case SAR: strcpy(nombre, "SAR"); break;
+        case LDL: strcpy(nombre, "LDL"); break;
+        case LDH: strcpy(nombre, "LDH"); break;
+        case RND: strcpy(nombre, "RND"); break;
+        default:  strcpy(nombre, "");
     }
 }
 
-static char *nombreRegistro(uint8_t codigo) {
+static void nombreRegistro(uint8_t codigo, st15 nombre) {
     switch (codigo) {
-        case EAX: return "EAX";
-        case EBX: return "EBX";
-        case ECX: return "ECX"; 
-        case EDX: return "EDX";
-        case IP:   return "IP";
-        case OPC:  return "OPC";
-        case OP1:  return "OP1";
-        case OP2:  return "OP2";
-        case LAR:  return "LAR";
-        case MAR:  return "MAR";
-        case MBR:  return "MBR";
-        case EEX:  return "EEX";
-        case EFX:  return "EFX";
-        case AC:   return "AC";
-        case CC:   return "CC"; 
-        case CS:   return "CS";
-        case DS:   return "DS";
-        default:   return "OPERANDO DESCONOCIDO";
+        case EAX: strcpy(nombre, "EAX"); break;
+        case EBX: strcpy(nombre, "EBX"); break;
+        case ECX: strcpy(nombre, "ECX"); break;
+        case EDX: strcpy(nombre, "EDX"); break;
+        case IP:   strcpy(nombre, "IP"); break;
+        case OPC:  strcpy(nombre, "OPC"); break;
+        case OP1:  strcpy(nombre, "OP1"); break;
+        case OP2:  strcpy(nombre, "OP2"); break;
+        case LAR:  strcpy(nombre, "LAR"); break;
+        case MAR:  strcpy(nombre, "MAR"); break;
+        case MBR:  strcpy(nombre, "MBR"); break;
+        case EEX:  strcpy(nombre, "EEX"); break;
+        case EFX:  strcpy(nombre, "EFX"); break;
+        case AC:   strcpy(nombre, "AC"); break;
+        case CC:   strcpy(nombre, "CC"); break;
+        case CS:   strcpy(nombre, "CS"); break;
+        case DS:   strcpy(nombre, "DS"); break;
+        default:   strcpy(nombre, "");
     }
 }
 
-static char *obtieneOperando(Operando OP) {
+static void obtieneOperando(Operando OP, st15 nombre) {
     
-    char *nombreoperando = NULL;
+    st15 nombreoperando;
     
     uint8_t tipo = OP >> 24; 
 
     switch (tipo) {
             case REGISTRO:
-                char *registro = nombreRegistro(OP & 0x1F);
+                st15 registro;
+                nombreRegistro(OP & 0x1F, registro);
                 strcpy(nombreoperando, registro);
 
                 break;
 
             case INMEDIATO:
                 int valor = OP & 0xFFFF;
-                sprintf(nombreoperando, "%d", valor);
+                char valor_str[12];
+                itoa(valor, valor_str, 10);
+                strcpy(nombreoperando, valor_str);
 
                 break;
 
             case MEMORIA:
                 strcpy(nombreoperando, "[");
 
-                strcat(nombreoperando, nombreRegistro(OP & 0x1F));
+                st15 registroMemoria;
+                nombreRegistro(OP & 0x1F, registroMemoria);
+                strcat(nombreoperando, registroMemoria);
                 
                 uint16_t offset = (OP >> 8) & 0xFFFF;
                 
                 if (offset != 0) {
                     strcat(nombreoperando, "+");
                     char offset_str[12];
-                    sprintf(offset_str, "%d", offset);
+                    itoa(offset, offset_str, 10);
                     strcat(nombreoperando, offset_str);
                 }
 
@@ -101,11 +107,11 @@ static char *obtieneOperando(Operando OP) {
                 break;
 
             default:
-                strcpy(nombreoperando, "OPERANDO DESCONOCIDO");
+                strcpy(nombreoperando, "");
                 break;
         }
 
-        return nombreoperando;
+    strcpy(nombre, nombreoperando);
 }
 
 void desensamblador(MaquinaVirtual maquina) {
@@ -113,7 +119,7 @@ void desensamblador(MaquinaVirtual maquina) {
     uint32_t inicio = maquina.segmentos[0].base;
     uint32_t fin = inicio + maquina.segmentos[0].tamanio;
     int desplazamiento;
-    char *operando1 = NULL, *operando2 = NULL, *operacion = NULL;
+    char *operando1, *operando2, *operacion;
 
     uint32_t direccionFisica = inicio;
     uint8_t instruccion;
@@ -127,11 +133,11 @@ void desensamblador(MaquinaVirtual maquina) {
 
         direccionFisica += desplazamiento + 1;
 
-        strcpy(operacion, nombreOperacion(maquina.registros[OPC]));
-        strcpy(operando1, obtieneOperando(maquina.registros[OP1]));
-        strcpy(operando2, obtieneOperando(maquina.registros[OP2]));
+        nombreOperacion(maquina.registros[OPC], operacion);
+        obtieneOperando(maquina.registros[OP1], operando1);
+        obtieneOperando(maquina.registros[OP2], operando2);
         
-        printf("0x%08X: 0x%08X 0x%08X 0x%08X   ||  %s, %s, %s\n", direccionFisica, maquina.registros[OPC], maquina.registros[OP1], maquina.registros[OP2], operacion, operando1, operando2);
+        printf("0x%08X: 0x%08X 0x%08X 0x%08X   ||  %s  %s, %s\n", direccionFisica, maquina.registros[OPC], maquina.registros[OP1], maquina.registros[OP2], operacion, operando1, operando2);
         
     }
         
